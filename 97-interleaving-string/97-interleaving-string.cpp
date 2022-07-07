88b83e1c2fd22d3fd5cb6e0dfbc7d7c2b08e83ff
Ajay Yadav <77874610+ajayyadavcstech@users.noreply.github.com>
2022-07-07 07:56:51 +0530

    Time: 0 ms (100.00%), Space: 7.2 MB (48.95%) - LeetHub

--- 97-interleaving-string/97-interleaving-string.cpp
+++ 97-interleaving-string/97-interleaving-string.cpp
@@ -3,7 +3,6 @@ public:
     vector<vector<int>> dp;
     bool Solve(string &s1,string &s2,string &s3,int i,int j,int k){
         if(i==s1.size() && j==s2.size() && k==s3.size()) return true;
-        if((i==s1.size() && j==s2.size() && k!=s3.size()) || (k==s3.size() && (i!=s1.size() || j!=s2.size())))  return false;
         if(dp[i][j]!=-1) return dp[i][j];
         bool isMatching = false;
         if(i!=s1.size() && j!=s2.size() && s1[i]==s3[k] && s2[j]==s3[k]){
@@ -20,6 +19,7 @@ public:
         
     }
     bool isInterleave(string s1, string s2, string s3) {
+        if(s1.size() + s2.size() !=s3.size()) return false;
         dp.resize(s1.size()+1,vector<int>(s2.size()+1,-1));
         return Solve(s1,s2,s3,0,0,0);
     }

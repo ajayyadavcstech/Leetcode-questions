b823d7e32bf4c67bf42f19fdc9bdf1485821799c
Ajay Yadav <77874610+ajayyadavcstech@users.noreply.github.com>
2022-07-14 08:10:16 +0530

    Time: 8 ms (75.11%), Space: 9.7 MB (45.63%) - LeetHub

--- 1089-duplicate-zeros/1089-duplicate-zeros.cpp
+++ 1089-duplicate-zeros/1089-duplicate-zeros.cpp
@@ -1,12 +1,23 @@
 class Solution {
 public:
     void duplicateZeros(vector<int>& arr) {
+        int no_of_zeroes = 0;
+        for(int i=0;i<arr.size();i++){
+            if(arr[i]==0) no_of_zeroes++;
+        }
         for(int i=arr.size()-1;i>=0;i--){
-            if(arr[i]==0){
-                for(int j=arr.size()-1;j>i;j--){
-                    arr[j] = arr[j-1];
+            if(arr[i]!=0 && i+no_of_zeroes<arr.size()){
+                arr[i+no_of_zeroes] = arr[i];
+            }
+            if(arr[i]==0) {
+                if(i+no_of_zeroes<arr.size()){
+                    arr[i+no_of_zeroes] = arr[i];
+                    arr[i+no_of_zeroes-1] = arr[i];
                 }
+                else if(i+no_of_zeroes==arr.size()) arr[i+no_of_zeroes-1] = arr[i];
+                no_of_zeroes--;
             }
         }
+        
     }
 };

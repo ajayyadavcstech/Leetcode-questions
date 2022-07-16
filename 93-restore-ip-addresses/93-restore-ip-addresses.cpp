926c3cd1b561ba194747d708a81be4dede9e754e
Ajay Yadav <77874610+ajayyadavcstech@users.noreply.github.com>
2022-07-16 20:13:52 +0530

    Time: 17 ms (8.99%), Space: 16.3 MB (5.01%) - LeetHub

---
+++ 93-restore-ip-addresses/93-restore-ip-addresses.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    vector<string> ans;
+    void solve(string &s,int i=0,int n=1,string temp=""){
+        if(n==5 && i==s.size()){
+            temp.pop_back();
+            ans.push_back(temp);
+            return ;
+        }
+        if(s[i]=='0'){
+            solve(s,i+1,n+1,temp+s.substr(i,1)+".");
+            return ;
+        }
+       if(i<s.size()){
+           solve(s,i+1,n+1,temp+s.substr(i,1)+".");
+       }
+       if(i+1<s.size()){
+           solve(s,i+2,n+1,temp+s.substr(i,2)+".");
+       }
+       if(i+2<s.size() && stoi(s.substr(i,3))<=255){
+           solve(s,i+3,n+1,temp+s.substr(i,3)+".");
+       }
+    }
+    vector<string> restoreIpAddresses(string s) {
+        solve(s);
+        return ans;
+    }
+};

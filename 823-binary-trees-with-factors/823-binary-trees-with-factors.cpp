510e9993cee1947c7f21c6abccbffd48a89084d9
Ajay Yadav <77874610+ajayyadavcstech@users.noreply.github.com>
2022-08-09 16:13:15 +0530

    Time: 134 ms (41.23%), Space: 8.5 MB (95.69%) - LeetHub

--- 823-binary-trees-with-factors/823-binary-trees-with-factors.cpp
+++ 823-binary-trees-with-factors/823-binary-trees-with-factors.cpp
@@ -1,34 +1,27 @@
 class Solution {
 public:
-    vector<int> dp;
     int mod = 1000000007;
-    long solve(vector<int>&arr,int ind){
-        if(ind==-1) return 0; 
-        if(dp[ind]!=-1) return dp[ind];
-        long cnt = 1;
-        int s = 0;
-        int e = ind-1;
-        while(s<=e){
-            long mul = (long)arr[s]*arr[e];
-            if(mul==arr[ind]){
-                long temp = (solve(arr,s) * solve(arr,e))%mod;
-                if(s!=e) temp=temp*2;
-                cnt = (cnt+temp)%mod;
-                s++;
-                e--;
-            }
-            else if(mul>arr[ind]) e--;
-            else s++;
-        }
-        return dp[ind] = cnt;
-    }
     int numFactoredBinaryTrees(vector<int>& arr) {
-       sort(arr.begin(),arr.end());
-      dp.resize(arr.size(),-1);
-        long cnt = 0;
+        sort(arr.begin(),arr.end());
+        vector<long> dp(arr.size(),1);
+        int ans = 0;
         for(int i=0;i<arr.size();i++){
-            cnt=(cnt+solve(arr,i))%mod;
+            int s = 0;
+            int e = i-1;
+            while(s<=e){
+                long mul = (long)arr[s]*arr[e];
+                if(mul==arr[i]){
+                    long cnt = (dp[s]*dp[e]) %mod;
+                    if(s!=e) cnt*=2;
+                    dp[i]= (dp[i]+cnt)%mod;
+                    e--;
+                    s++;
+                }
+                else if(mul>arr[i]) e--;
+                else s++;
+            }
+            ans=(ans+dp[i])%mod;
         }
-        return cnt;
+        return ans;
     }
 };

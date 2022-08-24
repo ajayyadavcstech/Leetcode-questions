df649424b71a1464f58c744e5a8dd5f5f286cb0e
Ajay Yadav <77874610+ajayyadavcstech@users.noreply.github.com>
2022-08-24 09:16:26 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (24.97%) - LeetHub

--- 342-power-of-four/342-power-of-four.cpp
+++ 342-power-of-four/342-power-of-four.cpp
@@ -2,10 +2,7 @@ class Solution {
 public:
     bool isPowerOfFour(int n) {
         if(n<1) return false;
-        while(n!=1){
-            if(n%4!=0) return false;
-            n/=4;
-        }
-        return true;
+        float pow = log2(n)/log2(4);
+        return ceil(pow)==floor(pow);
     }
 };

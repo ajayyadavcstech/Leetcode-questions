b1bf85dbf245766494f8cbccc108ecf171df3b94
Ajay Yadav <77874610+ajayyadavcstech@users.noreply.github.com>
2022-08-29 09:24:47 +0530

    Time: 38 ms (90.48%), Space: 18.1 MB (17.24%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.cpp
+++ 200-number-of-islands/200-number-of-islands.cpp
@@ -1,24 +1,45 @@
 class Solution {
 public:
-    void dfs(int i,int j,vector<vector<char>>& grid){
-        if(i==grid.size() || i<0 || j==grid[0].size() || j<0 || grid[i][j]=='0') return ;
-        grid[i][j]='0';
-        dfs(i+1,j,grid);
-        dfs(i-1,j,grid);
-        dfs(i,j+1,grid);
-        dfs(i,j-1,grid);
+    int fillWater(vector<vector<char>>&grid,int i,int j){
+        queue<pair<int,int>> q;
+        q.push({i,j});
+        grid[i][j] = '0';
         
-    }
-    int numIslands(vector<vector<char>>& grid) {
-        int ans=0;
-        for(int i=0;i<grid.size();i++){
-            for(int j=0;j<grid[0].size();j++){
-                if(grid[i][j]=='1'){
-                    dfs(i,j,grid);
-                    ans++;
-                }
+        while(q.size()){
+            pair<int,int> front = q.front();
+            q.pop();
+            int row = front.first;
+            int col = front.second;
+                
+            if(row+1<grid.size() && grid[row+1][col]=='1'){
+                q.push({row+1,col});
+                grid[row+1][col] = '0';
+            }
+                
+            if(row-1>=0 && grid[row-1][col]=='1'){
+                q.push({row-1,col});
+                grid[row-1][col] = '0';
+            }
+                
+            if(col+1<grid[0].size() && grid[row][col+1]=='1'){
+                q.push({row,col+1});
+                grid[row][col+1] = '0';
+            }
+                
+            if(col-1>=0 && grid[row][col-1]=='1'){
+                q.push({row,col-1});
+                grid[row][col-1] = '0';
             }
         }
-        return ans;
+        return 1;
+    }
+    int numIslands(vector<vector<char>>& grid) {
+        int totalIsland = 0;
+        for(int i=0;i<grid.size();i++){
+            for(int j=0;j<grid[0].size();j++){
+                if(grid[i][j]=='1') totalIsland+=fillWater(grid,i,j);
+            }
+        }
+        return totalIsland;
     }
 };

c8dd3faf38fda682f804d5d87ba196dda22b6129
Ajay Yadav <77874610+ajayyadavcstech@users.noreply.github.com>
2022-08-31 22:04:18 +0530

    Time: 577 ms (6.04%), Space: 19.9 MB (35.98%) - LeetHub

--- 417-pacific-atlantic-water-flow/417-pacific-atlantic-water-flow.cpp
+++ 417-pacific-atlantic-water-flow/417-pacific-atlantic-water-flow.cpp
@@ -2,12 +2,14 @@ class Solution {
 public:
     vector<int> Vx = {1,-1,0,0};
     vector<int> Vy = {0,0,1,-1};
-    vector<vector<int>> visited;
+    vector<vector<int>> visited;    
+    vector<vector<pair<int,pair<bool,bool>>>> dp;
     pair<bool,bool> isWay(vector<vector<int>>& heights,int i,int j,int pre){
         if(i==-1 || j==-1) return {true,false};
         if(i==heights.size() || j==heights[0].size()) return {false,true};
         if(pre<heights[i][j]) return {false,false};
         if(visited[i][j]) return {false,false};
+        if(dp[i][j].first==1) return dp[i][j].second;
         
         visited[i][j] = true;
         pair<bool,bool> ans = {false,false};
@@ -17,7 +19,8 @@ public:
             ans.second = ans.second || p.second;
             if(ans.first && ans.second) {
                 visited[i][j] = false;
-                return ans;
+                dp[i][j].first = 1;
+                return dp[i][j].second = ans;
             }
         }
         visited[i][j] = false;
@@ -26,7 +29,8 @@ public:
     vector<vector<int>> pacificAtlantic(vector<vector<int>>& heights) {
         int n = heights.size();
         int m = heights[0].size();
-        visited.resize(n,vector<int>(m,0));
+        visited.resize(n,vector<int>(m,0));        
+        dp.resize(n,vector<pair<int,pair<bool,bool>>>(m,{-1,{false,false}}));
         vector<vector<int>> ans;
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){

739dea25fbc8b8c1612eac5f8a142523f825621a
Ajay Yadav <77874610+ajayyadavcstech@users.noreply.github.com>
2022-09-07 20:01:26 +0530

    Time: 29 ms (69.10%), Space: 23.6 MB (70.62%) - LeetHub

--- 606-construct-string-from-binary-tree/606-construct-string-from-binary-tree.cpp
+++ 606-construct-string-from-binary-tree/606-construct-string-from-binary-tree.cpp
@@ -11,37 +11,25 @@
  */
 class Solution {
 public:
-    deque<string> st;
+    string ans;
     void solve(TreeNode* root){
-        if(!root) return;
+        if(!root) return ;
         
-        string num = to_string(root->val);
-        st.push_back(num);
-            
-        st.push_back("(");
-        solve(root->left);
-        st.push_back(")");
-        
-        st.push_back("(");
-        solve(root->right);
-        st.push_back(")");
-        
-        for(int i=0;i<2;i++){
-            string first = st.back();st.pop_back();
-            string second = st.back();st.pop_back();
-            
-            if(second=="(") continue;
-            st.push_back(second);
-            st.push_back(first);
+        ans+=to_string(root->val);
+        if(root->left){
+            ans+="(";
+            solve(root->left);
+            ans+=")";
+        }
+        if(root->right){
+            if(!root->left) ans+="()";
+             ans+="(";
+            solve(root->right);
+            ans+=")";
         }
     }
     string tree2str(TreeNode* root) {
-        string str;
         solve(root);
-        while(st.size()){
-            str.append(st.front());st.pop_front();
-        }
-        
-        return str;
+        return ans;
     }
 };

91a88750d72b8d97ec3edf7486bce1c5cdd35891
Ajay Yadav <77874610+ajayyadavcstech@users.noreply.github.com>
2022-10-19 11:28:33 +0530

    Time: 17 ms (78.27%), Space: 12.4 MB (88.37%) - LeetHub

--- 0692-top-k-frequent-words/0692-top-k-frequent-words.cpp
+++ 0692-top-k-frequent-words/0692-top-k-frequent-words.cpp
@@ -1,8 +1,8 @@
 class comp{
     public:
     bool operator()(pair<string,int>&a,pair<string,int>&b){
-        if(a.second!=b.second) return a.second<b.second;
-        else return a.first>b.first;
+        if(a.second!=b.second) return a.second>b.second;
+        else return a.first<b.first;
     }  
 };
 class Solution {
@@ -13,13 +13,22 @@ public:
         for(auto &x:words) mp[x]++;
         priority_queue<pair<string,int>,vector<pair<string,int>>,comp> pq;
         int cnt = 0;
-        for(auto &x:mp) pq.push(x);
+        for(auto &x:mp){
+            if(cnt<k){
+                cnt++;
+                pq.push(x);
+            }
+            else if(pq.top().second<x.second || (pq.top().second==x.second && pq.top().first>x.first)){
+                pq.pop();
+                pq.push(x);
+            }
+        }
         vector<string> ans;
-        while(k>0){
+        while(pq.size()){
             ans.push_back(pq.top().first);
             pq.pop();
-            k--;
         }
+        reverse(ans.begin(),ans.end());
         return ans;
     }
 };

2e3bbd4b91221ad4a7f5d03ab38843618a512dec
Ajay Yadav <77874610+ajayyadavcstech@users.noreply.github.com>
2022-10-30 00:04:13 +0530

    Time: 34 ms (96.69%), Space: 23.9 MB (79.78%) - LeetHub

--- 0503-next-greater-element-ii/0503-next-greater-element-ii.cpp
+++ 0503-next-greater-element-ii/0503-next-greater-element-ii.cpp
@@ -1,20 +1,20 @@
 class Solution {
 public:
     vector<int> nextGreaterElements(vector<int>& nums) {
-        priority_queue<vector<int>,vector<vector<int>>,greater<vector<int>>> pq;
+        stack<int> st;
         vector<int> ans(nums.size(),-1);
         for(int i=0;i<nums.size();i++){
-            while(pq.size() && pq.top()[0]<nums[i]){
-                ans[pq.top()[1]] = nums[i];
-                pq.pop();
-            }
-            pq.push({nums[i],i});
+            while(st.size() && nums[st.top()]<nums[i]){
+                ans[st.top()] = nums[i];
+                st.pop();
+             }
+            st.push(i);
         }
-        for(int i=0;i<nums.size();i++){
-           while(pq.size() && pq.top()[0]<nums[i]){
-                ans[pq.top()[1]] = nums[i];
-                pq.pop();
-            }
+         for(int i=0;i<nums.size();i++){
+            while(st.size() && nums[st.top()]<nums[i]){
+                ans[st.top()] = nums[i];
+                st.pop();
+             }
         }
         return ans;
     }

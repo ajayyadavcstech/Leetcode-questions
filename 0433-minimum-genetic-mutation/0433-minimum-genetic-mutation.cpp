04c6a20948f426e386eccdf1adbe245c56b6eb72
Ajay Yadav <77874610+ajayyadavcstech@users.noreply.github.com>
2022-11-02 10:35:45 +0530

    Time: 0 ms (100.00%), Space: 6.6 MB (42.79%) - LeetHub

--- 0433-minimum-genetic-mutation/0433-minimum-genetic-mutation.cpp
+++ 0433-minimum-genetic-mutation/0433-minimum-genetic-mutation.cpp
@@ -1,31 +1,34 @@
 class Solution {
 public:
-    vector<bool> visited;
-    int solve(string start,string end,vector<string>&bank){
-        if(start==end) return 0;
-        int ans = INT32_MAX;
-        for(int i=0;i<bank.size();i++){
-            if(visited[i]) continue;
-            bool check = true;
-            int cnt = 0;
-            for(int j=0;j<start.size();j++){
-                if(start[j]!=bank[i][j]) cnt++;
-                if(cnt>1) {
-                    check = false;
-                    break;
-                }
-            }
-            visited[i] = true;
-            if(check){
-                int val = solve(bank[i],end,bank);
-                if(val!=-1) ans = min(ans,1+val);
-            }
-            visited[i] = false;
+    int diff_char(string&s1,string&s2){
+        int cnt = 0;
+        for(int i=0;i<s1.size();i++){
+            if(s1[i]!=s2[i]) cnt++;
         }
-        return (ans==INT32_MAX ? -1 : ans);
+        return cnt;
     }
     int minMutation(string start, string end, vector<string>& bank) {
-        visited.resize(bank.size(),false);
-        return solve(start,end,bank);
+        queue<string> q;
+        vector<bool> visited(bank.size(),false);
+        q.push(start);
+        int step = 0;
+        while(q.size()){
+            int n = q.size();
+            
+            while(n--){
+                string cur = q.front();
+                if(cur==end) return step;
+                q.pop();
+                for(int i=0;i<bank.size();i++){
+                    if(visited[i]) continue;
+                    int cnt = diff_char(bank[i],cur);
+                    if(cnt<2){
+                        visited[i] = true;
+                        q.push(bank[i]);
+                    }
+                }
+            }step++;
+        }
+        return -1;
     }
 };

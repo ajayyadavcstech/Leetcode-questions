8495c269886e71e5af5ab81c20c16896c6c2bf9b
Ajay Yadav <77874610+ajayyadavcstech@users.noreply.github.com>
2023-01-17 14:30:56 +0530

    Time: 259 ms (12.15%), Space: 78.7 MB (11.64%) - LeetHub

--- 0926-flip-string-to-monotone-increasing/0926-flip-string-to-monotone-increasing.cpp
+++ 0926-flip-string-to-monotone-increasing/0926-flip-string-to-monotone-increasing.cpp
@@ -1,18 +1,20 @@
 class Solution {
 public:
-    vector<vector<int>> dp;
-    int solve(string&s,int ind,char pre){
-        if(ind==s.size()) return 0;
-        if(dp[ind][pre-'0']!=-1) return dp[ind][pre-'0'];
-        if(pre=='0'){
-            if(s[ind]=='0') return dp[ind][pre-'0']=min(solve(s,ind+1,pre),1+solve(s,ind+1,'1'));
-            else return dp[ind][pre-'0']=min(solve(s,ind+1,'1'),1+solve(s,ind+1,'0'));
-        }
-        if(s[ind]=='0') return dp[ind][pre-'0']=1 + solve(s,ind+1,'1');
-        return dp[ind][pre-'0']=solve(s,ind+1,'1');
-    }
     int minFlipsMonoIncr(string s) {
-        dp.resize(s.size()+1,vector<int>(2,-1));
-        return solve(s,0,'0');
+        int n = s.size();
+        vector<vector<int>> dp(n+1,vector<int>(2));
+        dp[n][0] = 0;
+        dp[n][1] = 0;
+        for(int i=n-1;i>=0;i--){
+            if(s[i]=='0'){
+                dp[i][0] = min(dp[i+1][0],1+dp[i+1][1]);
+                dp[i][1] = 1 + dp[i+1][1];
+            }
+            else{
+                dp[i][0] = min(dp[i+1][1],1+dp[i+1][0]);
+                dp[i][1] = dp[i+1][1];
+            }
+        }
+        return dp[0][0];
     }
 };

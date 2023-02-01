db3708d3ff219c8362472843adf2f11a716b187b
Ajay Yadav <77874610+ajayyadavcstech@users.noreply.github.com>
2023-02-01 13:43:45 +0530

    Time: 3 ms (83.39%), Space: 7.2 MB (43.04%) - LeetHub

--- 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.cpp
+++ 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.cpp
@@ -1,28 +1,7 @@
 class Solution {
 public:
-    bool solve(string s,string &str1,string&str2){
-        if(str1.size()%s.size()!=0 || str2.size()%s.size()!=0) return false;
-        int i = 0;
-        for(i=0;i<str1.size();){
-            for(int j=0;j<s.size();j++,i++){
-                if(str1[i]!=s[j] || str2[i]!=s[j]) return false;
-            }
-        }
-        for(;i<str2.size();){
-            for(int j=0;j<s.size();j++,i++){
-                if(str2[i]!=s[j]) return false;
-            }
-        }
-        return true;
-    }
     string gcdOfStrings(string str1, string str2) {
-        if(str1.size()>str2.size()) return gcdOfStrings(str2,str1);
-        string ans = "";
-        string temp = "";
-        for(int i=0;i<str1.size();i++){
-            temp.push_back(str1[i]);
-            if(solve(temp,str1,str2)) ans = temp;
-        }
-        return ans;
+        if(str1+str2 != str2+str1) return "";
+        return str1.substr(0,__gcd(str1.size(),str2.size()));
     }
 };

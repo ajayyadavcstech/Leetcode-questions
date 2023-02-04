bee6aaa93a0b022019a0bb3bba6364887422bdcd
Ajay Yadav <77874610+ajayyadavcstech@users.noreply.github.com>
2023-02-04 13:25:08 +0530

    Time: 10 ms (61.66%), Space: 7.2 MB (90.79%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.cpp
+++ 0567-permutation-in-string/0567-permutation-in-string.cpp
@@ -1,18 +1,19 @@
 class Solution {
 public:
-    bool is_substr(vector<int>&mp1,vector<int>&mp2){
-        for(int i=0;i<26;i++) if(mp1[i]!=mp2[i]) return false;
-        return true;
-    }
     bool checkInclusion(string s1, string s2) {
         vector<int> mp1(26,0);
         vector<int> mp2(26,0);
+        int cnt = 0;
         for(auto &x:s1) mp1[x-'a']++;
         for(int i=0;i<s2.size();i++){
-            mp2[s2[i]-'a']++;
+            int ind = s2[i]-'a';
+            mp2[ind]++;
+            if(mp2[ind]<=mp1[ind]) cnt++;
             if(i>=s1.size()-1){
-                if(is_substr(mp1,mp2)) return true;
-                mp2[s2[i-s1.size()+1]-'a']--;
+                if(cnt==s1.size()) return true;
+                ind = s2[i-s1.size()+1]-'a';
+                mp2[ind]--;
+                if(mp2[ind]<mp1[ind]) cnt--;
             }
         }
         return false;

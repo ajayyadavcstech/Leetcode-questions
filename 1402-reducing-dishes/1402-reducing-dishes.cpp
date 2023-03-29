d28677f56c1af32c373a66846e85f62deac99d69
Ajay Yadav <77874610+ajayyadavcstech@users.noreply.github.com>
2023-03-29 15:09:48 +0530

    Time: 7 ms (85.02%), Space: 8.1 MB (89.59%) - LeetHub

--- 1402-reducing-dishes/1402-reducing-dishes.cpp
+++ 1402-reducing-dishes/1402-reducing-dishes.cpp
@@ -1,14 +1,14 @@
 class Solution {
 public:
-    vector<vector<int>> dp;
-    int solve(int time,int i,vector<int>&sat){
-        if(i==sat.size()) return 0;
-        if(dp[time][i]!=-1) return dp[time][i];
-        return dp[time][i] = max(time*sat[i]+solve(time+1,i+1,sat),solve(time,i+1,sat));
-    }
     int maxSatisfaction(vector<int>& sat) {
-        dp.resize(sat.size()+1,vector<int>(sat.size()+1,-1));
-        sort(sat.begin(),sat.end());
-        return solve(1,0,sat);
+        sort(sat.begin(),sat.end(),greater<int>());
+        int ans = 0;
+        int cur_sum = 0;
+        for(int i=0;i<sat.size();i++){
+            cur_sum+=sat[i];
+            if(cur_sum<=0) break;
+            ans+=cur_sum;
+        }
+        return ans;
     }
 };

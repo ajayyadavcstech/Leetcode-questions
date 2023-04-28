4694727b928bc8e1b2598d07ae35e33de06f9f4a
Ajay Yadav <77874610+ajayyadavcstech@users.noreply.github.com>
2023-04-28 19:48:53 +0530

    Added solution - LeetHub

--- Geek's Village and Wells - GFG/geeks-village-and-wells.cpp
+++ Geek's Village and Wells - GFG/geeks-village-and-wells.cpp
@@ -13,12 +13,13 @@ public:
     vector<vector<int>> chefAndWells(int n,int m,vector<vector<char>> &c){
         // Code here
         vector<vector<int>> rc = {{1,-1,0,0},{0,0,1,-1}};
+        vector<vector<bool>> vis(n,vector<bool>(m,false));
         queue<pair<int,int>> q;
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
                 if(c[i][j]=='W'){ 
                     q.push({i,j});
-                    c[i][j] = 'N';
+                    vis[i][j] = true;
                 }
             }
         }
@@ -26,20 +27,20 @@ public:
         int stp = 0;
         while(q.size()){
             int size = q.size();
-            stp++;
             while(size--){
                 int i = q.front().first;
                 int j = q.front().second;
+                if(c[i][j]=='H') ans[i][j] = 2*stp;
                 q.pop();
                 for(int k=0;k<4;k++){
                     int row = rc[0][k] + i;
                     int col = rc[1][k] + j;
-                    if(row<0 || row==n || col<0 || col==m || c[row][col]=='N') continue;
-                    if(c[row][col]=='H') ans[row][col] = 2*stp;
-                    c[row][col] = 'N';
+                    if(row<0 || row==n || col<0 || col==m || vis[row][col] || c[row][col]=='N') continue;
+                    vis[row][col] = true;
                     q.push({row,col});
                 }
             }
+            stp++;
         }
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
